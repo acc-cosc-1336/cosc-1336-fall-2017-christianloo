e713d2d487509e46a361be5be463dfecb26f1872
christianloo <31975390+christianloo@users.noreply.github.com>
2017-11-22 17:11:23 -0600

    Add files via upload

---
+++ course.py
@@ -0,0 +1,7 @@
+class Course:
+
+    def __init__(self, course_id, title, credit_hour):
+
+        self.course_id = course_id
+        self.title = title
+        self.credit_hour = credit_hour
---
+++ student.py
@@ -0,0 +1,8 @@
+class Student:
+
+    def __init__(self, student_id, first_name, last_name, enroll_date):
+
+        self.student_id = student_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.enroll_date = enroll_date
